af49ec8ad6900af0dfb26ce65e530e149be525a6
yigithankardas <y.kardas@etu.edu.tr>
2023-12-01 11:17:17 +0300

    CHG: For #2, Namings and typings have been refactored. ADD: For #2, Signal handling for CTRL+C has been added. REM: For #2, Unneccessary lock statements have been removed. ADD: For #2, Client ports have been stored in dictionary.

--- src/main.py
+++ src/main.py
@@ -1,63 +1,60 @@
 import socket
 import random
 import threading
+import time
+import signal
+import os
+import sys
 
-# Server ayarları
+
 HOST = '127.0.0.1'
 PORT = 5555
 
-# Bağlı kullanıcıları tutacak dictionary
-connected_clients = {}
+clients = {}
 
-lock = threading.Lock()
-number_of_clients = 0
+lock: threading.Lock = threading.Lock()
+numberOfClients = 0
 
-def generate_unique_id():
+
+def generateUniqueId() -> int:
     while True:
-        new_id = random.randint(100, 999999)
-        with lock:
-            # Yeni ID dictionary'de var mı kontrol et, yoksa atamayı yap
-            if new_id not in connected_clients:
-                return new_id
+        newId = random.randint(100, 999999)
+        if newId not in clients:
+            return newId
 
-def handle_client(client_socket, client_address):
-    # Unique ID oluştur
-    global number_of_clients
-    unique_id = generate_unique_id()
-    number_of_clients += 1
 
-    with lock:
-        # Client'a ID'yi gönder
-        client_socket.send(str(unique_id).encode())
+def handleClient(clientSocket: socket.socket, clientAddress: tuple) -> None:
+    global numberOfClients
+    uniqueId = generateUniqueId()
+    numberOfClients += 1
 
-        # Dictionary'e ekle
-        connected_clients[unique_id] = {'ip': client_address, 'state': 'connected'}
+    clientSocket.send(str(uniqueId).encode(encoding='utf-8'))
 
-    print(f"New connection from {client_address}. Assigned ID: {unique_id}. Number of clients is {number_of_clients}")
+    clients[uniqueId] = {'ip': clientAddress[0],
+                         'port': clientAddress[1], 'state': 'connected'}
 
-    # YİĞİDO AŞAĞISI ÇALIŞMIYOR ASLA. Whilede bişiler yazmadım daha. Belki bunla alakalıdır
-    # ama ctrl+c ile çıkınca clienttan bu finallye falanda girmiyor. Bu yüzden client sayısı da azalmıyor.
-    # server ctrl+c ile kapanmıyor client bağlansada bağlanmasada anlamadım
-    '''try:
-        while True:
-            # Burada bağlı kalındığı sürece işlemleri gerçekleştirebilirsiniz
-            pass
-    except KeyboardInterrupt:
-        print(f"Connection with ID {unique_id} closed.")
-    finally:
-        # Bağlantı sona erdiğinde dictionary'den kaldır ve Client sayısını bir azalt.
-        with lock:
-            number_of_clients -= 1
-            del connected_clients[unique_id]'''
+    print(
+        f"New connection from {clientAddress}. Assigned ID: {uniqueId}. Number of clients is {numberOfClients}")
+
+    while True:
+        print(f'handling {uniqueId}')
+        time.sleep(1)
+
 
 if __name__ == "__main__":
-    server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    server_socket.bind((HOST, PORT))
-    server_socket.listen()
+    def signalHandler(signal, frame):
+        serverSocket.close()
+        sys.exit(1)
+
+    signal.signal(signal.SIGINT, signalHandler)
+    serverSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    serverSocket.bind((HOST, PORT))
+    serverSocket.listen()
 
     print(f"Server listening on {HOST}:{PORT}")
 
     while True:
-        client_socket, client_address = server_socket.accept()
-        client_thread = threading.Thread(target=handle_client, args=(client_socket, client_address))
-        client_thread.start()
+        clientSocket, clientAddr = serverSocket.accept()
+        clientThread = threading.Thread(
+            target=handleClient, args=(clientSocket, clientAddr))
+        clientThread.start()
